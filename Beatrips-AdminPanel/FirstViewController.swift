41a38f2f9320569e247464ec616fe28818620111
Burak Uzunboy <burak@beatrips.com>
2017-08-02 20:50:47 +0300

    Initial Commit

---
+++ Beatrips-AdminPanel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Beatrips-AdminPanel
+//
+//  Created by Burak Uzunboy on 2.08.2017.
+//  Copyright © 2017 Burak Uzunboy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
