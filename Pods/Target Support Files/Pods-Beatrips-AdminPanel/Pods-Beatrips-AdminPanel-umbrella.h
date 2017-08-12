21ce64a055fc9deac2da102f730fe319f91c1a46
Burak Uzunboy <burak@beatrips.com>
2017-08-12 11:52:55 +0300

    Missed files

---
+++ Pods/Target Support Files/Pods-Beatrips-AdminPanel/Pods-Beatrips-AdminPanel-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Beatrips_AdminPanelVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Beatrips_AdminPanelVersionString[];
+
